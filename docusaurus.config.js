b7365c0369ab6bf468b764d1b0dd17f093bc02d9
seria <seria.ati@gmail.com>
2025-11-15 23:56:24 +0900

    Update site URL in Docusaurus configuration (#100)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://hb-docs.seria.moe',
+  url: 'https://docs.hb.seria.moe',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
